01f2519834075dbe53a05f935883c8113c6a8211
Marco Pagani <marco@pagani.io>
2025-03-08 18:04:47 -0500

    decent responsiveness

--- frontend/src/lib/message-ticker.svelte
+++ frontend/src/lib/message-ticker.svelte
@@ -1,79 +1,79 @@
 <script lang="ts">
-  
-  let paperHolder: HTMLDivElement;
-  let paperContent: HTMLDivElement;
+	let paperHolder: HTMLDivElement;
+	let paperContent: HTMLDivElement;
 
-  let { messages } = $props()
+	let { messages } = $props();
 
-  $effect(() => {
-    messages.length
-    requestAnimationFrame(() => {
-      if (paperHolder) {
-        paperHolder.style.maxHeight = `${paperContent.clientHeight + 15}px`;
-      }
-    });
-  })
-
+	$effect(() => {
+		messages.length;
+		requestAnimationFrame(() => {
+			if (paperHolder) {
+				paperHolder.style.maxHeight = `${paperContent.clientHeight + 15}px`;
+			}
+		});
+	});
 </script>
-<div>
-<div class="h-24 slot mt-18"></div>
-<div class="relative bg-gray-600 h-2 border-gray-500 border-y -mt-13 mx-4 mb-24">
-  <div id="paper-holder" bind:this={paperHolder}>
-    <div id="paper" bind:this={paperContent} >
-      {#each messages as message}
-      {#if message}
-        <div class="p-4 font-receipt text-black text-sm text-left flex flex-col gap-2">
-          <p>Message sent!
-          <p >From {message.sender || 'Anonymous'} @ {(new Date()).toLocaleDateString()}</p>
-          <p>{message.content}</p>
-          <p>--------------------------------</p>
-        </div>
-        {:else}
-<div class="h-24"></div>
-        {/if}
-      {/each}
-    </div>
-  </div>
-</div>
+
+<div class="w-full">
+	<div class="slot sm:mt-16 h-24"></div>
+	<div class="relative mx-4 -mt-13 mb-24 h-2 rounded border-y-2 border-gray-500 bg-gray-600">
+		<div id="paper-holder" bind:this={paperHolder}>
+			<div id="paper" bind:this={paperContent}>
+				{#each messages as message}
+					{#if message}
+						<div class="font-receipt flex flex-col gap-2 p-4 text-left text-sm text-black">
+							<p>Message sent!</p>
+							<p>From {message.sender || 'Anonymous'} @ {new Date().toLocaleTimeString()}</p>
+							<p>{message.content}</p>
+							<p>--------------------------------</p>
+						</div>
+					{:else}
+						<div class="h-24"></div>
+					{/if}
+				{/each}
+			</div>
+		</div>
+	</div>
 </div>
 
 <style lang="css">
+	.slot {
+		width: 100%;
+		--box-shadow-settings:
+			-8px -8px 12px rgba(255, 255, 255, 0.3), 8px 8px 12px rgba(30, 31, 32, 0.897);
+		position: relative;
+		background-color: rgba(0, 0, 0, 0.3);
+		border: 0.3rem solid rgb(90, 90, 90);
+		border-radius: 0.6rem/.6rem;
+		box-shadow: var(--box-shadow-settings);
+	}
+	.slot::after {
+		content: '';
+		position: absolute;
+		height: 100%;
+		width: 100%;
+		border-radius: 0.4rem/.4rem;
+		box-shadow: inset var(--box-shadow-settings);
+	}
 
-  .slot {
-    --box-shadow-settings: -8px -8px 12px rgba(255,255,255,0.3), 8px 8px 12px rgba(30, 31, 32, 0.897);
-    position: relative;
-    background-color: rgba(0,0,0,0.3);
-    border: .3rem solid rgb(90, 90, 90);
-    border-radius: .6rem/.6rem;
-    box-shadow: var(--box-shadow-settings);
-  }
-  .slot::after {
-    content: '';
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    border-radius: .4rem/.4rem;
-    box-shadow: inset var(--box-shadow-settings);
-  }
-
-  #paper-holder {
-    display: flex;
-    align-items: flex-end;
-    overflow: hidden;
-    top: 3px;
-    left: 0;
-    right: 0;
-    max-height: 0; /* Start with zero height */
-    transition:max-height 2s steps(20,end);
-    padding-bottom: 15px;
-  }
-#paper {
-    width: 350px;
-    margin: 0 auto; /* Center the content */
-    height: auto; /* Set height to auto */
-    line-height: 30px;
-    text-align: center;
-    background-color: white;
-    box-shadow: 3px 3px 10px rgba(0,0,0,0.5), inset 0px 5px 12px -5px rgba(0,0,0,0.5);
-  }
+	#paper-holder {
+		width: 100%;
+    margin-top:2px;
+		display: flex;
+		align-items: flex-end;
+		overflow: hidden;
+		max-height: 0;
+		transition: max-height 2s steps(20, end);
+		padding-bottom: 15px;
+	}
+	#paper {
+		width: 95%;
+		margin: 0 auto; 
+		line-height: 30px;
+		text-align: center;
+		background-color: white;
+		box-shadow:
+			3px 3px 10px rgba(0, 0, 0, 0.5),
+			inset 0px 5px 12px -5px rgba(0, 0, 0, 0.5);
+	}
 </style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,33 +1,31 @@
 <script lang="ts">
 	import { supabase } from '$lib/supabase';
-  import MessageTicker from '$lib/message-ticker.svelte';
+	import MessageTicker from '$lib/message-ticker.svelte';
 
-  type Message = {
-    content: string;
-    sender: string;
-  }
+	type Message = {
+		content: string;
+		sender: string;
+	};
 
-let print = false;
+	let print = false;
 
-const submittedMessages: (Message | boolean)[] = $state([])
+	const submittedMessages: (Message | boolean)[] = $state([]);
 
 	let message = $state('');
 	let sender = $state('');
 
-
 	let success = false;
 	let errorMsg = '';
 
-  function feed() {
-    submittedMessages.unshift(false)
-  }
+	function feed() {
+		submittedMessages.unshift(false);
+	}
 
 	async function submitMessage() {
+		submittedMessages.unshift({ content: message, sender: sender });
 
-    submittedMessages.unshift({content: message, sender: sender})
-
-    // print = true;
-    // receipt = message
+		// print = true;
+		// receipt = message
 		// success = false;
 		// errorMsg = '';
 
@@ -35,8 +33,6 @@ const submittedMessages: (Message | boolean)[] = $state([])
 		// 	errorMsg = 'Message cannot be empty!';
 		// 	return;
 		// }
-
-  
 
 		// const { error } = await supabase.from('messages').insert([
 		// 	{ content: message.trim(), sender: sender.trim() } // 'status' is optional, defaults to 'pending'
@@ -52,64 +48,82 @@ const submittedMessages: (Message | boolean)[] = $state([])
 	}
 </script>
 
-<div class="flex h-full flex-col items-center p-6 font-sans min-w-[556px]">
+<div class="flex h-full  flex-col items-center p-2 sm:p-6 font-sans">
+	<div
+		class="font-receipt mt-8 mb-14 flex -rotate-3 flex-col items-center bg-white p-6 text-gray-800 drop-shadow-md"
+	>
+		<h1 class="pb-2 text-3xl">paper.pagani.io</h1>
+		<p class="">A silly “fax machine” just for me</p>
+	</div>
 
-  <div class="bg-white  p-6  flex flex-col items-center text-gray-800 font-receipt -rotate-3 my-8 drop-shadow-md">
-    <h1 class="text-3xl pb-2 ">paper.pagani.io</h1>
-    <p class="">A silly “fax machine” just for me</p>
-    </div>
-
-	<form onsubmit={submitMessage} class="w-full max-w-xl border-black rounded-lg border-6 p-10 bg-gray-800 text-amber-100">
-    <p class="pt-4 pb-8 ">If you send me a message it will appear in my house. So be nice.<br>Also, paper costs money so keep it short, thanks.</p>
+	<form
+		onsubmit={submitMessage}
+		class="w-full max-w-xl rounded-lg border-6 border-black bg-gray-800 p-10 text-amber-100"
+	>
+		<p class="pt-4 pb-8">
+			If you send me a message it will appear in my house. So be nice.<br />Also, paper costs money
+			so keep it short, thanks.
+		</p>
 		<div class="w-full pb-6">
 			<label
-				class="block pb-2 text-sm font-semibold  after:text-red-700 after:content-['*']"
-				for="message-input">
-        Write your message
-        </label>
+				class="block pb-2 text-sm font-semibold after:text-red-700 after:content-['*']"
+				for="message-input"
+			>
+				Write your message
+			</label>
 			<textarea
-        bind:value={message}
+				bind:value={message}
 				id="message-input"
-				class="h-24 w-full resize-none rounded-lg border-gray-300 focus:border-amber-800 text-black"
+				class="h-24 w-full resize-none rounded-lg border-gray-300 text-black focus:border-amber-800"
 				maxlength="120"
 				required
 			></textarea>
 		</div>
-		<div class="flex justify-between gap-6 ">
-			<div class="self-start w-full xs:w-auto">
-				<label class="block pb-2 text-sm font-semibold " for="sender-input">
-          Who sent it?
-        </label>
-				<input bind:value={sender}  class="rounded-lg border-gray-300 focus:border-amber-800 w-full xs:w-auto text-black" id="sender-input" />
+		<div class="flex flex-col sm:flex-row justify-between gap-6 items-baseline">
+			<div class="xs:w-auto w-full self-start">
+				<label class="block pb-2 text-sm font-semibold" for="sender-input"> Who sent it? </label>
+				<input
+					bind:value={sender}
+					class="xs:w-auto w-full rounded-lg border-gray-300 text-black focus:border-amber-800"
+					id="sender-input"
+				/>
 			</div>
 
-			<button type="submit" class="self-end rounded bg-amber-900 p-3 font-bold text-white hover:bg-amber-950 w-full xs:w-auto">
-        Print Message
-        </button>
+			<button
+				type="submit"
+				class="xs:w-auto w-full self-end rounded-lg border-amber-900 border-4 p-3 font-bold text-white hover:bg-amber-950"
+			>
+				Print Message
+			</button>
 		</div>
- 
-    <div class="flex mt-12" >
-      <div role="presentation" class="-ml-10 mr-10 bg-black rounded-r-xl border-2 border-gray-900 p-5 font-semibold flex flex-col gap-2 items-center border-l-0">
-        <div class="w-5 h-5 bg-blue-900 rounded-full flex items-center justify-center text-white"></div>
-        <p>POWER</p>
-        <div class="w-5 h-5 bg-gray-900 rounded-full flex items-center justify-center text-white"></div>
-        <p>ERROR</p>
-        <div class="w-5 h-5 bg-green-900 rounded-full flex items-center justify-center text-white"></div>
-        <p>PAPER</p>
-        <button type="button" onclick={feed} class="w-18 h-12 hover:bg-gray-600 rounded-lg text-black bg-gray-400">FEED</button>
-      </div>
-    <MessageTicker messages={submittedMessages} />
-    </div>
 
-		{#if success}
-			<p class="success">Message sent successfully!</p>
-		{/if}
-		{#if errorMsg}
-			<p class="error">{errorMsg}</p>
-		{/if}
+		<div class="mt-10 flex flex-col sm:flex-row gap-10">
+			<div
+				role="presentation"
+				class=" -ml-10 flex sm:flex-col  self-start items-center justify-center gap-2 rounded-r-xl border-2 border-l-0 border-gray-900 bg-black p-4 font-semibold text-xs"
+			>
+				<div
+					class="flex h-4 w-4 items-center justify-center rounded-full bg-blue-900 text-white"
+				></div>
+				<p>POWER</p>
+				<div
+					class="flex h-4 w-4 items-center justify-center rounded-full bg-gray-900 text-white"
+				></div>
+				<p>ERROR</p>
+				<div
+					class="flex h-4 w-4 items-center justify-center rounded-full bg-green-900 text-white"
+				></div>
+				<p>PAPER</p>
+				<button
+					type="button"
+					onclick={feed}
+					class="h-10 w-14 rounded-lg bg-gray-400 text-black hover:bg-gray-600 text-sm cursor-pointer">FEED</button
+				>
+			</div>
+			<MessageTicker messages={submittedMessages} />
+		</div>
 	</form>
 </div>
 
 <style lang="css">
-
 </style>
